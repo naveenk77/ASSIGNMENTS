277ce5acb8348f1e22b22f05e7cdf3c34327c325
Naveen Konda <92906665+naveenk77@users.noreply.github.com>
2021-11-17 21:40:34 +0530

    Add files via upload

---
+++ SPRING REST/ASSIGNMENT 1/src/main/java/com/naveen/SpringRest/SpringRestApplication.java
@@ -0,0 +1,15 @@
+package com.naveen.SpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.naveen.Book.Example1;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApplication.class, args);
+	}
+
+}
