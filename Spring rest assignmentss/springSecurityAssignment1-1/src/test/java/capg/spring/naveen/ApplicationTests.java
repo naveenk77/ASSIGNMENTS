13d70e113316800e73e61ba15e7661cca316289f
Naveen Konda <92906665+naveenk77@users.noreply.github.com>
2021-11-29 11:49:18 +0530

    Add files via upload

---
+++ Spring rest assignmentss/springSecurityAssignment1-1/src/test/java/capg/spring/naveen/ApplicationTests.java
@@ -0,0 +1,13 @@
+package capg.spring.naveen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
